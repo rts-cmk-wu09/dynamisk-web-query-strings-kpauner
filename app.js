e9967a916455c141bce4df10b8ff1a0af02a9a1d
kpauner <kpauner@gmail.com>
2023-03-02 16:22:10 +0100

    add likes

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const destList = document.createElement('ul');
 destList.classList.add('listing');
 body.append(destList);
 
-//add eventlistener load?
-let isFavorite = JSON.parse(localStorage.getItem('isFavorite'));
-
 
 fetch('./data/destinations.json')
     .then(response => response.json())
     .then(data => {
         const destinations = data.destinations;
         destinations.forEach(destination => {
+            let isFavorite = JSON.parse(localStorage.getItem(`${destination.id}`));
             const destItem = document.createElement('li'); // list
             destList.append(destItem);
             const destImg = document.createElement('img'); // img
@@ -24,25 +22,32 @@ fetch('./data/destinations.json')
             moreBar.classList.add('more');
             destItem.append(moreBar);
             const favLink = document.createElement('a'); // fav
-            favLink.setAttribute('href', '#')
+            favLink.setAttribute('id', `${destination.id}`);
             favLink.classList.add('favorite');
-            favLink.classList.toggle(isFavorite === true ? 'fa-solid' : 'fa-regular');
+            if (isFavorite) {
+                favLink.classList.add('fa-solid');
+                favLink.classList.remove('fa-regular');
+              } else {
+                favLink.classList.remove('fa-solid');
+                favLink.classList.add('fa-regular');
+              }
+            // Maybe use the destination ID to distinguish between listitems? target-parentNode
             moreBar.append(favLink);
             const destLink = document.createElement('a'); // destination link
             destLink.setAttribute('href', `destinations.html?id=${destination.id}`)
             destLink.classList.add('dest-link');
             moreBar.append(destLink);
             const destMore = document.createTextNode('more')
-            destLink.append(destMore);  
+            destLink.append(destMore); 
+            
 
             // FAVORITE
-            favLink.addEventListener("click", e => {
-                e.preventDefault();
-                favLink.classList.toggle('fa-regular')
-                favLink.classList.toggle('fa-solid')
-                isFavorite = !isFavorite; 
-                localStorage.setItem('isFavorite', isFavorite); 
-            })
+            favLink.addEventListener("click", e => {          
+                isFavorite = !isFavorite;
+                favLink.classList.toggle('fa-solid');
+                favLink.classList.toggle('fa-regular');
+                localStorage.setItem(`${destination.id}`, isFavorite); 
+              })
         });         
     })
     .catch(error => console.error(error));
--- fetchdestination.js
+++ fetchdestination.js
@@ -4,25 +4,59 @@ const params = new URLSearchParams(window.location.search);
 const destinationId = params.get('id') ?? 'default';
 
 fetch(`./data/${destinationId}.json`)
-.then(response => response.json())
-.then(destinationData => {
-    body.innerHTML = `
-    <article class="dest">
-        <div class="dest-img">
-            <a href="#" class="favorite"><span class="fa-regular fa-heart fa-lg"></span>favorit</a>
-            <img src="img/${destinationData.image}" class="pre-img" alt="">
-        </div>
-        <div class="content">
-            <a href="!#" class="category">${destinationData.destination}</a>
-            <h1>${destinationData.title}</h1>
-            <p class="subtitle">${destinationData.subtitle}</p>
-            <p>${destinationData.text}</p>
-            <h2>Facilities</h2>
-            <ul>
-                ${destinationData.facilities.map(facility => `<li>${facility}</li>`).join('')}
-            </ul>
-        </div>
-    </article>
+.then(response => response.json()) // Promise handler
+.then(data => {
+    let isFavorite = JSON.parse(localStorage.getItem(`${data.id}`));
+    const destItem = document.createElement('article'); // article
+    destItem.classList.add('dest');
+    body.append(destItem);
+
+    const imgContainer = document.createElement('div'); // img Container
+    imgContainer.classList.add('dest-img');
+    destItem.append(imgContainer);
+
+    const destImg = document.createElement('img'); // img
+    destImg.setAttribute('src', `img/${data.image}`); 
+    imgContainer.append(destImg);
+
+    const favContainer = document.createElement('span'); // fav Container
+    favContainer.classList.add('favcontainer');
+    imgContainer.append(favContainer);
+
+    const favLink = document.createElement('a'); // fav
+    favLink.setAttribute('id', `${data.id}`);
+    favLink.classList.add('favorite');
+    if (isFavorite) {
+        favLink.classList.add('fa-solid');
+        favLink.classList.remove('fa-regular');
+        } else {
+        favLink.classList.remove('fa-solid');
+        favLink.classList.add('fa-regular');
+    }
+    favContainer.append(favLink);
+
+    const destMore = document.createTextNode('favorite')
+    favLink.append(destMore);
+
+    const content = document.createElement('div'); // img Container
+    content.classList.add('content');
+    destItem.append(content);
+    content.innerHTML = `
+    <a href="!#" class="category">${data.destination}</a>
+        <h1>${data.title}</h1>
+        <p class="subtitle">${data.subtitle}</p>
+        <p>${data.text}</p>
+        <h2>Facilities</h2>
+        <ul>
+        ${data.facilities.map(facility => `<li>${facility}</li>`).join('')}
+    </ul>
     `;
+
+    favLink.addEventListener("click", e => {          
+        isFavorite = !isFavorite;
+        favLink.classList.toggle('fa-solid');
+        favLink.classList.toggle('fa-regular');
+        localStorage.setItem(`${data.id}`, isFavorite); 
+    })
 })
 .catch(error => console.error(error));
